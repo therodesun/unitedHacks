4f3bb6ea34be8188d206986a29b62f92d8d2668f
Vishnu Reshamwala <vishnureshamwala@gmail.com>
2025-01-17 22:22:18 -0800

    added more fields on signup

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,49 +1,82 @@
-import React from 'react'
-import './LoginSignup.css'
-import {useState} from 'react'
-
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
+import React, { useState } from 'react';
+import './LoginSignup.css';
+import { FaUser, FaGraduationCap, FaBriefcase, FaLock } from 'react-icons/fa';
+import { MdEmail, MdWorkOutline } from 'react-icons/md';
+import { GiRank3 } from 'react-icons/gi';
 
 const LoginSignup = () => {
-
     const [action, setAction] = useState("Sign Up");
 
-
-
-
-
-
-
     return (
-        <div className = 'container'>
-            <div className = "header">
-                <div className = "text">{action}</div>
-                <div className = "underline"></div>
+        <div className="container">
+            <div className="header">
+                <div className="text">{action}</div>
+                <div className="underline"></div>
             </div>
-        <div className = "inputs">
-            {action==="Login"?<div></div>: <div className = "input">
-            <img src={user_icon} alt="" />
-            <input type="text" placeholder="Name" />
-        </div>}
-       
-        <div className = "input">
-            <img src={email_icon} alt="" />
-            <input type="email" placeholder="Email"/>
-        </div>  
-        <div className = "input">
-            <img src={password_icon} alt="" />
-            <input type="password" placeholder="Password"/>
-        </div>  
-        </div>
-        {action==="Sign Up"?<div></div>:<div className = "forgot-password">Forgot Password? <span>Click Here!</span></div>}
-       
-        <div className = "submit-container">
-            <div className = {action==="Login"?"submit gray":"submit"} onClick = {()=> {setAction("Sign Up")}}>Sign Up</div>
-            <div className = {action==="Sign Up"?"submit gray":"submit" } onClick = {() => {setAction("Login")}}>Login</div>
-        </div>
-
+            <div className="inputs">
+                {action === "Sign Up" && (
+                    <>
+                        <div className="input">
+                            <FaUser size={24} color="#797979" style={{ margin: '0 30px' }} />
+                            <input type="text" placeholder="First Name" />
+                        </div>
+                        <div className="input">
+                            <FaUser size={24} color="#797979" style={{ margin: '0 30px' }} />
+                            <input type="text" placeholder="Last Name" />
+                        </div>
+                        <div className="input">
+                            <GiRank3 size={24} color="#797979" style={{ margin: '0 30px' }} />
+                            <select>
+                                <option value="recruiter">Recruiter</option>
+                                <option value="employee">Employee</option>
+                            </select>
+                        </div>
+                        <div className="input">
+                            <MdWorkOutline size={24} color="#797979" style={{ margin: '0 30px' }} />
+                            <input type="text" placeholder="Field" />
+                        </div>
+                        <div className="input">
+                            <FaGraduationCap size={24} color="#797979" style={{ margin: '0 30px' }} />
+                            <input type="text" placeholder="Education" />
+                        </div>
+                        <div className="input">
+                            <FaBriefcase size={24} color="#797979" style={{ margin: '0 30px' }} />
+                            <input type="text" placeholder="Experience" />
+                        </div>
+                    </>
+                )}
+                <div className="input">
+                    <MdEmail size={24} color="#797979" style={{ margin: '0 30px' }} />
+                    <input type="email" placeholder="Email" />
+                </div>
+                <div className="input">
+                    <FaLock size={24} color="#797979" style={{ margin: '0 30px' }} />
+                    <input type="password" placeholder="Password" />
+                </div>
+            </div>
+            {action === "Login" && (
+                <div className="forgot-password">
+                    Forgot Password? <span>Click Here!</span>
+                </div>
+            )}
+            <div className="submit-container">
+                <div
+                    className={action === "Login" ? "submit gray" : "submit"}
+                    onClick={() => {
+                        setAction("Sign Up");
+                    }}
+                >
+                    Sign Up
+                </div>
+                <div
+                    className={action === "Sign Up" ? "submit gray" : "submit"}
+                    onClick={() => {
+                        setAction("Login");
+                    }}
+                >
+                    Login
+                </div>
+            </div>
         </div>
     );
 };
