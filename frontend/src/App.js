1bb34d93fba49933e211580e770f28d238898e7b
Vishnu Reshamwala <vishnureshamwala@gmail.com>
2025-01-18 16:38:18 -0800

    Created Main Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
-
+import MainPage from './Components/MainPage/MainPage';
 
 function App() {
+  // Dummy user data
+  const dummyUser = {
+    firstName: "Alice",
+    lastName: "Johnson",
+    education: "B.Sc. in Computer Science",
+    skills: "React, Node.js, Python",
+    experience: "3 years",
+    location: "New York",
+    salary: "$80,000/year",
+    profileImage: "https://i.pravatar.cc/150?img=8" // Reliable online profile pic
+  };
+
   return (
     <div>
-      <LoginSignup/>
+      <MainPage user={dummyUser} />
     </div>
   );
 }
