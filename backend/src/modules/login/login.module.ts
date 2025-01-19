ac2a750ffb7e54f15fbdd2ac8fbb1fd5d4a3a23c
therodesun <49075133+therodesun@users.noreply.github.com>
2025-01-18 17:39:38 -0800

    loging endpoint

--- backend/src/modules/login/login.module.ts
+++ backend/src/modules/login/login.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { LoginController } from './controllers/login.controller';
 import { LoginService } from './services/login.service';
 import { LoginDto } from './types/dtos/login.dto';  // Correct path to DTO
+import { TokenModule } from 'src/modules/token/token.module'; 
 
 @Module({
   controllers: [LoginController],
+  imports: [TokenModule], 
   providers: [LoginService],
 })
 export class LoginModule {}
--- backend/src/modules/login/services/login.service.ts
+++ backend/src/modules/login/services/login.service.ts
@@ -1,22 +1,39 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { DbContext } from 'src/modules/database/context/db-context';
-import { LoginDto } from '../types/dtos/login.dto'; 
+import { LoginDto } from '../types/dtos/login.dto';
+import { HttpException } from '@nestjs/common';
+import { TokenService } from 'src/modules/token/services/token.service'; 
 
+@Injectable()
 export class LoginService {
-  constructor(private readonly dbContext: DbContext) {}
+  constructor(
+    private readonly dbContext: DbContext,
+    private readonly tokenService: TokenService, 
+  ) {}
 
-  async login(loginDto: LoginDto): Promise<string> {
+  async login(loginDto: LoginDto): Promise<any> {
     const { email, password } = loginDto;
-    const user = await this.dbContext.users.findOne({ email });
 
-    if (!user) {
-      throw new HttpException('Invalid credentials', 400); 
+    try {
+      const user = await this.dbContext.users.findOne({ email });
+      if (!user) {
+        throw new HttpException('Invalid credentials', 400);
+      }
+
+      if (user.password !== password) {
+        throw new HttpException('Invalid credentials', 400);
+      }
+
+      const  {accessToken} = await this.tokenService.generateUserTokens(user); 
+
+      return {
+        message: 'Login successful',
+        accessToken,
+      
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Internal server error', 500);
     }
-
-    if (user.password !== password) {
-      throw new HttpException('Invalid credentials', 400); 
-    }
-
-    return 'Login successful'; 
-}
+  }
 }
